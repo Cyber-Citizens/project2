b2f29fe119e699c15c5777962a8eb410008dea3e
sbowma15 <70663678+sbowma15@users.noreply.github.com>
2023-02-22 20:56:06 -0700

    Add files via upload
    Added Doc Strings for each function

--- project2.py
+++ project2.py
@@ -2,6 +2,16 @@ import re
 
 
 def check_indentation(code):
+    """
+    Given a list of strings containing code, checks and fixes indentation errors.
+
+    Args:
+        code (list): A list of strings representing the code to be checked.
+
+    Returns:
+        str: The fixed code as a single string with proper indentation.
+
+    """
     indentation_level = 0
     fixed_code = []
     in_definition = False
@@ -38,6 +48,129 @@ def check_indentation(code):
 
 
 def check_function_header(code):
+    """
+    Given a list of strings containing code, checks and fixes function header errors.
+
+    Args:
+        code (list): A list of strings representing the code to be checked.
+
+    Returns:
+        str: The fixed code as a single string with correct function headers.
+
+    """
+For the count_print_keyword function:
+
+python
+Copy code
+def count_print_keyword(code):
+    """
+    Given a string containing code, counts the number of times the `print()` keyword is used.
+
+    Args:
+        code (str): A string representing the code to be analyzed.
+
+    Returns:
+        int: The number of times the `print()` keyword is used.
+
+    """
+For the process_code function:
+
+python
+Copy code
+def process_code(code):
+    """
+    Given a list of strings containing code, processes the code by checking and fixing indentation
+    and function header errors, and counting the number of times the `print()` keyword is used.
+
+    Args:
+        code (list): A list of strings representing the code to be processed.
+
+    Returns:
+        tuple: A tuple containing the fixed code as a single string with proper indentation and
+        function headers, and the number of times the `print()` keyword is used.
+
+    """
+For the write_to_file function:
+
+less
+Copy code
+def write_to_file(input_code, output_code, print_count):
+    """
+    Given a string representing the input code, a string representing the fixed code, and an integer
+    representing the number of times the `print()` keyword is used, writes the original code,
+    updated code, and print count to a file named "output.txt".
+
+    Args:
+        input_code (str): A string representing the original input code.
+        output_code (str): A string representing the fixed code with proper indentation and
+        function headers.
+        print_count (int): An integer representing the number of times the `print()` keyword is used.
+
+    Returns:
+        None.
+
+    """
+
+
+
+Shane Bowman
+write doc strings for each function and send me the entire code back in one segment:
+
+def check_indentation(code):
+    """
+    This function checks the indentation of the code and adds necessary indentation to the code.
+    
+    Parameters:
+    code (str): The code to be processed.
+    
+    Returns:
+    str: The processed code with the necessary indentation.
+    """
+    indentation_level = 0
+    fixed_code = []
+    in_definition = False
+    for line in range(len(code)):
+        stripped_line = code[line].strip()
+        if stripped_line.startswith("def"):
+            in_definition = True
+            fixed_line = "\t" * indentation_level + stripped_line + "\n"
+            fixed_code.append(fixed_line)
+            indentation_level += 1
+        elif stripped_line.endswith(":") or stripped_line.startswith("if") \
+            or stripped_line.startswith("else") or \
+                stripped_line.startswith("elif"):
+            fixed_line = "\t" * indentation_level + stripped_line + "\n"
+            fixed_code.append(fixed_line)
+            indentation_level += 1
+        elif stripped_line.startswith("return"):
+            indentation_level = 1
+            fixed_line = "\t" * indentation_level + stripped_line + "\n"
+            fixed_code.append(fixed_line)
+            in_definition = False
+        elif stripped_line == "":
+            indentation_level = 0
+            fixed_line = "\n"
+            fixed_code.append(fixed_line)
+        else:
+            fixed_line = "\t" * indentation_level + stripped_line + "\n"
+            fixed_code.append(fixed_line)
+            if in_definition:
+                indentation_level = 1
+            else:
+                indentation_level = 0
+    return "".join(fixed_code)
+
+
+def check_function_header(code):
+    """
+    This function checks the function headers and corrects them if necessary.
+    
+    Parameters:
+    code (str): The code to be processed.
+    
+    Returns:
+    str: The processed code with the corrected function headers.
+    """
     fixed_code = []
     for line in range(len(code)):
         if code[line].startswith("def"):
@@ -53,6 +186,15 @@ def check_function_header(code):
 
 
 def count_print_keyword(code):
+    """
+    This function counts the number of times the `print()` keyword is used in the code.
+    
+    Parameters:
+    code (str): The code to be processed.
+    
+    Returns:
+    int: The number of times the `print()` keyword is used in the code.
+    """
     # Remove comments and strings to avoid counting `print()` within them
     code = re.sub(r'(\".*?\"|\'.*?\')|(#.*)', '', code, flags=re.DOTALL)
 
@@ -69,6 +211,180 @@ def count_print_keyword(code):
 
 
 def process_code(code):
+    """
+    This function processes the given code by checking its indentation, function headers, and counting the
+    number of times the `print()` keyword is used.
+    
+    Parameters:
+    code (str): The code to be processed.
+    
+    Returns:
+    tuple: A tuple containing the processed code and the number of times the `print()` keyword is used.
+    """
+   
+import re
+
+def check_indentation(code):
+"""
+This function checks the indentation of the code and adds necessary indentation to the code.
+
+vbnet
+Copy code
+Parameters:
+code (str): The code to be processed.
+
+Returns:
+str: The processed code with the necessary indentation.
+"""
+indentation_level = 0
+fixed_code = []
+in_definition = False
+for line in range(len(code)):
+    stripped_line = code[line].strip()
+    if stripped_line.startswith("def"):
+        in_definition = True
+        fixed_line = "\t" * indentation_level + stripped_line + "\n"
+        fixed_code.append(fixed_line)
+        indentation_level += 1
+    elif stripped_line.endswith(":") or stripped_line.startswith("if") \
+        or stripped_line.startswith("else") or \
+            stripped_line.startswith("elif"):
+        fixed_line = "\t" * indentation_level + stripped_line + "\n"
+        fixed_code.append(fixed_line)
+        indentation_level += 1
+    elif stripped_line.startswith("return"):
+        indentation_level = 1
+        fixed_line = "\t" * indentation_level + stripped_line + "\n"
+        fixed_code.append(fixed_line)
+        in_definition = False
+    elif stripped_line == "":
+        indentation_level = 0
+        fixed_line = "\n"
+        fixed_code.append(fixed_line)
+    else:
+        fixed_line = "\t" * indentation_level + stripped_line + "\n"
+        fixed_code.append(fixed_line)
+        if in_definition:
+            indentation_level = 1
+        else:
+            indentation_level = 0
+return "".join(fixed_code)
+def check_function_header(code):
+"""
+This function checks the function headers and corrects them if necessary.
+
+lua
+Copy code
+Parameters:
+code (str): The code to be processed.
+
+Returns:
+str: The processed code with the corrected function headers.
+"""
+fixed_code = []
+for line in range(len(code)):
+    if code[line].startswith("def"):
+        match = re.search("def (\w+)\((.*)\):", code[line])
+        if match:
+            function_name = match.group(1)
+            arguments = match.group(2)
+            fixed_line = f"def {function_name}({arguments}):"
+            fixed_code.append(fixed_line)
+    else:
+        fixed_code.append(code[line])
+return "".join(fixed_code)
+def count_print_keyword(code):
+"""
+This function counts the number of times the print() keyword is used in the code.
+
+swift
+Copy code
+Parameters:
+code (str): The code to be processed.
+
+Returns:
+int: The number of times the `print()` keyword is used in the code.
+"""
+# Remove comments and strings to avoid counting `print()` within them
+code = re.sub(r'(\".*?\"|\'.*?\')|(#.*)', '', code, flags=re.DOTALL)
+
+# Split the code into lines
+lines = code.split('\n')
+
+# Count the number of times `print()` is called
+count = 0
+for line in lines:
+    if 'print(' in line and not 'print(' in line.split('print(')[-1]:
+        count += 1
+
+return count
+def process_code(code):
+"""
+This function processes the given code by checking its indentation, function headers, and counting the
+number of times the print() keyword is used.
+
+vbnet
+Copy code
+Parameters:
+code (str): The code to be processed.
+
+Returns:
+tuple: A tuple containing the processed code and the number of times the `print()` keyword is used.
+"""
+processed_code = check_indentation(code)
+    fixed_code = []
+    for line in range(len(code)):
+        if code[line].startswith("def"):
+            match = re.search("def (\w+)\((.*)\):", code[line])
+            if match:
+                function_name = match.group(1)
+                arguments = match.group(2)
+                fixed_line = f"def {function_name}({arguments}):"
+                fixed_code.append(fixed_line)
+        else:
+            fixed_code.append(code[line])
+    return "".join(fixed_code)
+
+
+def count_print_keyword(code):
+    """
+    Given a string containing code, counts the number of times the `print()` keyword is used.
+
+    Args:
+        code (str): A string representing the code to be analyzed.
+
+    Returns:
+        int: The number of times the `print()` keyword is used.
+
+    """
+    # Remove comments and strings to avoid counting `print()` within them
+    code = re.sub(r'(\".*?\"|\'.*?\')|(#.*)', '', code, flags=re.DOTALL)
+
+    # Split the code into lines
+    lines = code.split('\n')
+
+    # Count the number of times `print()` is called
+    count = 0
+    for line in lines:
+        if 'print(' in line and not 'print(' in line.split('print(')[-1]:
+            count += 1
+
+    return count
+
+
+def process_code(code):
+    """
+    Given a list of strings containing code, processes the code by checking and fixing indentation
+    and function header errors, and counting the number of times the `print()` keyword is used.
+
+    Args:
+        code (list): A list of strings representing the code to be processed.
+
+    Returns:
+        tuple: A tuple containing the fixed code as a single string with proper indentation and
+        function headers, and the number of times the `print()` keyword is used.
+
+    """
     fixed_code = check_indentation(code)
     fixed_code = check_function_header(fixed_code)
     print_count = count_print_keyword(fixed_code)
@@ -76,6 +392,21 @@ def process_code(code):
 
 
 def write_to_file(input_code, output_code, print_count):
+     """
+    Given a string representing the input code, a string representing the fixed code, and an integer
+    representing the number of times the `print()` keyword is used, writes the original code,
+    updated code, and print count to a file named "output.txt".
+
+    Args:
+        input_code (str): A string representing the original input code.
+        output_code (str): A string representing the fixed code with proper indentation and
+        function headers.
+        print_count (int): An integer representing the number of times the `print()` keyword is used.
+
+    Returns:
+        None.
+
+    """
     with open("output.txt", "w") as f:
         f.write("Original code:\n")
         f.write(input_code)
@@ -88,8 +419,8 @@ def write_to_file(input_code, output_code, print_count):
 
 
 if __name__ == "__main__":
-    codef = open("testExamples.py", "r")
-    code = codef.readlines()
+    with open("testExamples.py", "r", encoding="utf-8") as codef:
+        code = codef.readlines()
     fixed_code, print_count = process_code(code)
     write_to_file("".join(code), fixed_code, print_count)
     print("Processing complete. Check 'output.txt' for the results.")
